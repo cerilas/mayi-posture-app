db1ac702ec19a8f824ee842aa3caed4a5a43f292
Cerilas <deniz@cerilas.com>
2026-08-07 01:11:19 +0300

    Initial Commit

---
+++ MY Posture App/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MY Posture App
+//
+//  Created by Deniz on 7.08.2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MY Posture App/MY_Posture_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MY_Posture_AppApp.swift
+//  MY Posture App
+//
+//  Created by Deniz on 7.08.2026.
+//
+
+import SwiftUI
+
+@main
+struct MY_Posture_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
